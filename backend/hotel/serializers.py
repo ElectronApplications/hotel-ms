93b308b3c95d67f98a7b4c10011a6ff50028b870
ElectronApps <barbwirek4@gmail.com>
2024-10-02 16:17:12 +0800

    Simple API implementation for all models (to be reworked)

--- backend/hotel/serializers.py
+++ backend/hotel/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
 
+from hotel.models.accomodation import Accomodation, ServiceCard
+from hotel.models.service import Service
+from hotel.models.place import ClassInfo, Place, Room
 from hotel.models.client import Client
 from hotel.models.reservation import Reservation
 
@@ -8,9 +11,37 @@ class ClientSerializer(serializers.ModelSerializer):
         model = Client
         fields = ["id", "name", "phone_number"]
 
+class ClassInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ClassInfo
+        fields = ["id", "class_description", "place_price"]
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ["id", "room_class"]
+
+class PlaceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Place
+        fields = ["id", "room"]
+
+class ServiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Service
+        fields = ["id", "service_description", "service_price", "classes"]
+
 class ReservationSerializer(serializers.ModelSerializer):
-    client = ClientSerializer()
-    
     class Meta:
         model = Reservation
-        fields = ["id", "move_in_time", "move_out_time", "client"]
+        fields = ["id", "move_in_time", "move_out_time", "client", "places"]
+
+class AccomodationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Accomodation
+        fields = ["id", "move_in_time", "move_out_time", "client", "places", "price_to_pay", "was_price_paid", "checked_out", "reservation"]
+
+class ServiceCardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ServiceCard
+        fields = ["id", "service", "accomodation", "price_to_pay", "was_price_paid", "service_time"]
