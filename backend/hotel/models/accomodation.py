3110aac36ee4617be1f3c49f73284431309ef407
ElectronApps <klimchikeg@gmail.com>
2024-10-02 15:11:12 +0800

    Imrove admin panel experience a bit

--- backend/hotel/models/accomodation.py
+++ backend/hotel/models/accomodation.py
@@ -15,6 +15,9 @@ class Accomodation(models.Model):
     places = models.ManyToManyField(Place, related_name="accomodations")
     reservation = models.OneToOneField(Reservation, on_delete=models.SET_NULL, null=True, blank=True, related_name="accomodation")
 
+    def __str__(self) -> str:
+        return f"Проживание №{self.id} на {self.client.name}"
+
     class Meta:
         verbose_name = "Проживание"
         verbose_name_plural = "Проживания"
@@ -25,6 +28,9 @@ class ServiceCard(models.Model):
     price_to_pay = models.DecimalField("Стоимость услуги", max_digits=8, decimal_places=2)
     service_time = models.DateTimeField("Время предоставления услуги")
     was_price_paid = models.BooleanField("Оплачена ли услуга", default=False)
+
+    def __str__(self) -> str:
+        return f"Карточка услуг №{self.id} на {self.accomodation.client.name} в проживании №{self.accomodation.id}"
 
     class Meta:
         verbose_name = "Карточка услуг"
