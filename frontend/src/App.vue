e2d392b61d6309e9280dc3ab92be8d1e7a5a6bd5
ElectronApps <barbwirek4@gmail.com>
2024-10-16 12:58:39 +0800

    Implement simple headerbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,30 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import { Bars3Icon } from "@heroicons/vue/24/outline";
+</script>
 
 <template>
-  <div></div>
+  <header>
+    <nav
+      class="container sticky mx-auto flex flex-row justify-between border-b p-4"
+    >
+      <RouterLink to="/" class="text-sm font-bold">
+        Hotel Management System
+      </RouterLink>
+
+      <button class="block md:hidden">
+        <span class="sr-only">Open menu</span>
+        <Bars3Icon class="h-6 w-6" aria-hidden="true" />
+      </button>
+
+      <div
+        class="hidden flex-row gap-x-8 text-sm font-semibold text-gray-900 md:flex"
+      >
+        <a href="/admin">Django admin page</a>
+        <RouterLink to="/vue-admin">Admin page</RouterLink>
+      </div>
+    </nav>
+  </header>
+
+  <RouterView />
 </template>
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,27 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
     proxy: {
-      '/api': {
-        target: "http://localhost:8000"
-      }
-    }
-  }
-})
+      "/api": {
+        target: "http://localhost:8000",
+      },
+      "/admin": {
+        target: "http://localhost:8000",
+      },
+      "/static": {
+        target: "http://localhost:8000",
+      },
+    },
+  },
+});
