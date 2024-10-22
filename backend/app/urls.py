7f90df5815e5d6776bc6c8bd43e293cdb7182f76
ElectronApps <barbwirek4@gmail.com>
2024-10-22 23:24:31 +0800

    Add docker support for production deployment

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from rest_framework.routers import DefaultRouter
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -27,4 +29,4 @@ register_hotel_routes(router)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
