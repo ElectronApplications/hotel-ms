809b1d51c43a2fcd92cfa9e5eb9efd5954ab3ee4
ElectronApps <barbwirek4@gmail.com>
2024-10-23 15:09:29 +0800

    Implement a simple admin page

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,1 +1,14 @@
-<template>hii main</template>
+<script setup lang="ts">
+const BRAND_HOTEL_NAME = import.meta.env.VITE_BRAND_HOTEL_NAME;
+</script>
+
+<template>
+  <main class="container mx-auto pt-5">
+    <h1 class="text-4xl font-extrabold">
+      Welcome to the hotel {{ BRAND_HOTEL_NAME }}!
+    </h1>
+    <h2 class="text-2xl font-bold">
+      We are going to provide you with the best hotel experience!
+    </h2>
+  </main>
+</template>
