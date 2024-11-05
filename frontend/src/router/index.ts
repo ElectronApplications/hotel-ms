53fec939c9af8506e1ee1bf27d917655910dad96
ElectronApps <barbwirek4@gmail.com>
2024-11-05 14:00:45 +0800

    Implement authentication on frontend

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MainView from "@/views/MainView.vue";
-import AdminView from "@/views/AdminView.vue";
+import LoginView from "@/views/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,8 +10,8 @@ const router = createRouter({
       component: MainView,
     },
     {
-      path: "/vue-admin",
-      component: AdminView,
+      path: "/login",
+      component: LoginView,
     },
   ],
 });
