68b0ebea573d89b9752e2d0a232617b6b9c15d78
ElectronApps <klimchikeg@gmail.com>
2024-11-29 18:37:58 +0800

    Unlocalize

--- backend/hotel/models/accomodation.py
+++ backend/hotel/models/accomodation.py
@@ -6,31 +6,31 @@ from hotel.models.reservation import Reservation
 
 class Accomodation(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, null=False, related_name="accomodations")
-    price_to_pay = models.DecimalField("Стоимость проживания", max_digits=8, decimal_places=2)
-    move_in_time = models.DateTimeField("Время въезда")
-    move_out_time = models.DateTimeField("Время выезда")
-    was_price_paid = models.BooleanField("Оплачено ли проживание", default=False)
-    checked_out = models.BooleanField("Уже выехали", default=False)
+    price_to_pay = models.DecimalField("Accomodation's price", max_digits=8, decimal_places=2)
+    move_in_time = models.DateTimeField("Time of moving in")
+    move_out_time = models.DateTimeField("Time of moving out")
+    was_price_paid = models.BooleanField("Did the client pay", default=False)
+    checked_out = models.BooleanField("Did the client check out", default=False)
     places = models.ManyToManyField(Place, related_name="accomodations")
     reservation = models.OneToOneField(Reservation, on_delete=models.SET_NULL, null=True, blank=True, related_name="accomodation")
 
     def __str__(self) -> str:
-        return f"Проживание №{self.id} на {self.client.name}"
+        return f"Accomodation №{self.id} for {self.client.name}"
 
     class Meta:
-        verbose_name = "Проживание"
-        verbose_name_plural = "Проживания"
+        verbose_name = "Accomodation"
+        verbose_name_plural = "Accomodations"
 
 class ServiceCard(models.Model):
     service = models.ForeignKey(Service, on_delete=models.CASCADE, null=False, related_name="servicecards")
     accomodation = models.ForeignKey(Accomodation, on_delete=models.CASCADE, null=False, related_name="servicecards")
-    price_to_pay = models.DecimalField("Стоимость услуги", max_digits=8, decimal_places=2)
-    service_time = models.DateTimeField("Время предоставления услуги")
-    was_price_paid = models.BooleanField("Оплачена ли услуга", default=False)
+    price_to_pay = models.DecimalField("Service's price", max_digits=8, decimal_places=2)
+    service_time = models.DateTimeField("Time of service")
+    was_price_paid = models.BooleanField("Did the client pay", default=False)
 
     def __str__(self) -> str:
-        return f"Карточка услуг №{self.id} на {self.accomodation.client.name} в проживании №{self.accomodation.id}"
+        return f"Service card №{self.id} for {self.accomodation.client.name} and accomodation №{self.accomodation.id}"
 
     class Meta:
-        verbose_name = "Карточка услуг"
-        verbose_name_plural = "Карточки услуг"
+        verbose_name = "Service card"
+        verbose_name_plural = "Service cards"
