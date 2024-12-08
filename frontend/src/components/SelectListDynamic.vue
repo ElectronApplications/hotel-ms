c2952281c3305af64809e9a527d7ad8053c1a310
ElectronApps <klimchikeg@gmail.com>
2024-12-08 12:44:19 +0800

    Implement adding and deleting clients in the admin page

--- frontend/src/components/SelectListDynamic.vue
+++ frontend/src/components/SelectListDynamic.vue
@@ -1,30 +1,42 @@
 <script setup lang="ts">
-import { toRefs, watch } from "vue";
+import { nextTick, ref, toRefs, watch } from "vue";
 
 const props = defineProps<{
   options: readonly string[];
-  defaultOption?: number;
+  selected: number;
 }>();
 
-const { options, defaultOption } = toRefs(props);
+const { options, selected } = toRefs(props);
 
-const model = defineModel<number>();
+const emit = defineEmits<{
+  updateSelection: [id: number];
+}>();
+
+const selectedOption = ref(0);
 
 watch(
-  defaultOption,
+  selected,
   (value) => {
-    model.value = value;
+    selectedOption.value = value;
   },
   {
     immediate: true,
   },
 );
+
+function updateSelection() {
+  emit("updateSelection", selectedOption.value);
+  nextTick(() => {
+    selectedOption.value = selected.value;
+  });
+}
 </script>
 
 <template>
   <select
     class="rounded-md border-0 bg-surface-light p-2 py-2 shadow-sm ring-1 ring-secondary-light hover:cursor-pointer sm:text-sm/6 dark:bg-surface-dark dark:ring-secondary-active-dark"
-    v-model="model"
+    @change.prevent="updateSelection"
+    v-model="selectedOption"
   >
     <option
       v-for="[index, option] in options.entries()"
--- frontend/src/views/AdminView.vue
+++ frontend/src/views/AdminView.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
 import DefaultProfileImage from "@/assets/default-profile.png";
+import EditableLabel from "@/components/EditableLabel.vue";
+import ExpandableImage from "@/components/ExpandableImage.vue";
+import PrimaryButton from "@/components/PrimaryButton.vue";
+import SelectList from "@/components/SelectList.vue";
+import SelectListDynamic from "@/components/SelectListDynamic.vue";
+import TextField from "@/components/TextField.vue";
 import { useAuthentication, useUserRole } from "@/composables/auth";
 import router from "@/router";
 import { clientRoles, type Client, type ClientRole } from "@/types";
+import { XMarkIcon } from "@heroicons/vue/24/outline";
 import axios from "axios";
 import { onMounted, ref } from "vue";
-import ExpandableImage from "@/components/ExpandableImage.vue";
-import EditableLabel from "@/components/EditableLabel.vue";
-import SelectList from "@/components/SelectList.vue";
 
 const clients = ref<Client[]>([]);
 
@@ -42,6 +46,11 @@ async function changeClientRole(client: Client, newRole: ClientRole) {
   }
 }
 
+async function deleteClient(client: Client) {
+  await axios.delete(`/api/client/${client.id}/`);
+  await fetchClients();
+}
+
 useAuthentication((isAuthenticated) => {
   if (!isAuthenticated) {
     router.push("/login");
@@ -53,6 +62,29 @@ useUserRole((role) => {
     router.push("/profile");
   }
 });
+
+const newClientName = ref("");
+const newClientPhoneNumber = ref("");
+const newClientRole = ref<number>(clientRoles.indexOf("client"));
+
+async function createClient(): Promise<boolean> {
+  if (newClientName.value === "" || newClientPhoneNumber.value === "") {
+    return false;
+  }
+
+  await axios.post("/api/client/", {
+    name: newClientName.value,
+    phone_number: newClientPhoneNumber.value,
+    role: clientRoles[newClientRole.value],
+  });
+  await fetchClients();
+
+  newClientName.value = "";
+  newClientPhoneNumber.value = "";
+  newClientRole.value = clientRoles.indexOf("client");
+
+  return true;
+}
 
 onMounted(async () => {
   await fetchClients();
@@ -67,6 +99,8 @@ onMounted(async () => {
     <div
       class="mt-4 rounded-3xl bg-slate-100 px-0 py-6 text-surface-content-light shadow-md dark:bg-secondary-dark dark:text-surface-content-dark"
     >
+      <form id="createClientForm" @submit.prevent="createClient" />
+
       <table
         class="w-full table-auto bg-slate-100 text-center dark:bg-secondary-dark"
       >
@@ -74,6 +108,7 @@ onMounted(async () => {
           class="border-b border-secondary-light dark:border-secondary-active-dark"
         >
           <tr>
+            <th></th>
             <th class="py-2">Name</th>
             <th class="py-2">Phone number</th>
             <th class="py-2">Role</th>
@@ -82,6 +117,17 @@ onMounted(async () => {
         </thead>
         <tbody class="bg-surface-light dark:bg-surface-dark">
           <tr v-for="client in clients" v-bind:key="client.id">
+            <td
+              class="border-b border-secondary-light py-2 dark:border-secondary-active-dark"
+            >
+              <button
+                class="rounded-md bg-red-500 p-[4px]"
+                v-if="client.role !== 'admin'"
+                @click="deleteClient(client)"
+              >
+                <XMarkIcon class="h-[24px] w-[24px] text-white" />
+              </button>
+            </td>
             <td
               class="border-b border-secondary-light py-6 dark:border-secondary-active-dark"
             >
@@ -111,7 +157,7 @@ onMounted(async () => {
             <td
               class="border-b border-secondary-light py-6 dark:border-secondary-active-dark"
             >
-              <SelectList
+              <SelectListDynamic
                 v-if="client.role !== 'admin'"
                 :options="clientRoles"
                 :selected="clientRoles.indexOf(client.role)"
@@ -131,6 +177,51 @@ onMounted(async () => {
                 :src="client.picture ?? DefaultProfileImage"
                 :alt="client.name"
               />
+            </td>
+          </tr>
+
+          <tr>
+            <td
+              class="border-b border-secondary-light py-2 dark:border-secondary-active-dark"
+            ></td>
+            <td
+              class="border-b border-secondary-light py-6 dark:border-secondary-active-dark"
+            >
+              <div class="inline-block w-[100px] lg:w-[250px]">
+                <TextField
+                  placeholder="Client's name"
+                  v-model="newClientName"
+                  form="createClientForm"
+                />
+              </div>
+            </td>
+            <td
+              class="border-b border-secondary-light py-6 dark:border-secondary-active-dark"
+            >
+              <div class="inline-block w-[100px] lg:w-[250px]">
+                <TextField
+                  placeholder="Client's phone number"
+                  v-model="newClientPhoneNumber"
+                  form="createClientForm"
+                />
+              </div>
+            </td>
+            <td
+              class="border-b border-secondary-light py-6 dark:border-secondary-active-dark"
+            >
+              <SelectList
+                :options="clientRoles"
+                :defaultOption="clientRoles.indexOf('client')"
+                v-model="newClientRole"
+                form="createClientForm"
+              />
+            </td>
+            <td
+              class="border-b border-secondary-light py-6 dark:border-secondary-active-dark"
+            >
+              <PrimaryButton form="createClientForm" type="submit"
+                >Create new client</PrimaryButton
+              >
             </td>
           </tr>
         </tbody>
