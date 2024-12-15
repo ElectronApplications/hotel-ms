6e2fc18fe9d05c2f5cd58a124b33c95c65203e5a
ElectronApps <barbwirek4@gmail.com>
2024-12-15 23:54:13 +0800

    Implement showing rooms on the main page

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,18 +1,31 @@
 <script setup lang="ts">
 import BackgroundImage from "@/assets/mainview-background.png";
+import GalleryCarousel from "@/components/GalleryCarousel.vue";
 import PrimaryButton from "@/components/PrimaryButton.vue";
 import SurfaceCard from "@/components/SurfaceCard.vue";
 import { useAuthStore } from "@/stores/auth";
-import type { Room } from "@/types";
+import type { Class, Room } from "@/types";
 import { TransitionRoot } from "@headlessui/vue";
 import axios from "axios";
 import { storeToRefs } from "pinia";
-import { onBeforeMount, ref } from "vue";
+import { computed, onBeforeMount, ref } from "vue";
 import { RouterLink } from "vue-router";
 
 const BRAND_HOTEL_NAME = import.meta.env.VITE_BRAND_HOTEL_NAME;
+const CURRENCY_SYMBOL = import.meta.env.VITE_CURRENCY_SYMBOL;
 
 const rooms = ref<Room[]>([]);
+const classes = ref<Class[]>([]);
+
+const displayRooms = computed(() =>
+  rooms.value
+    .filter((x) => x.status == "free")
+    .map((x) => {
+      let room = x as Room & { class_info: Class };
+      room.class_info = classes.value.find((y) => y.id == x.room_class)!;
+      return room;
+    }),
+);
 
 const authStore = useAuthStore();
 const { currentUser } = storeToRefs(authStore);
@@ -24,6 +37,7 @@ onBeforeMount(async () => {
   }, 1000);
 
   rooms.value = (await axios.get("/api/room/")).data;
+  classes.value = (await axios.get("/api/class/")).data;
 });
 </script>
 
@@ -68,7 +82,46 @@ onBeforeMount(async () => {
       <SurfaceCard
         class="rounded-t-none rounded-tr-none text-center lg:rounded-tl-3xl"
       >
-        <span>Some content here!!!</span>
+        <div class="flex flex-row flex-wrap gap-y-4">
+          <div
+            v-for="room in displayRooms"
+            v-bind:key="room.id"
+            class="flex basis-full flex-col items-center md:basis-1/2 lg:basis-1/3 xl:basis-1/4"
+          >
+            <div
+              v-if="
+                room.class_info.gallery !== null &&
+                room.class_info.gallery.images.length !== 0
+              "
+              class="h-[200px] w-[300px]"
+            >
+              <GalleryCarousel
+                :gallery="room.class_info.gallery"
+                imgClass="rounded-xl w-[300px] h-[200px] object-cover"
+              />
+            </div>
+            <div
+              v-else
+              class="h-[200px] w-[300px] rounded-xl bg-secondary-light dark:bg-secondary-dark"
+            />
+
+            <div class="pt-2 text-2xl">
+              {{ room.class_info.class_description }} - {{ room.places }} places
+            </div>
+
+            <div>
+              Starting at {{ room.class_info.place_price * room.places }}
+              {{ CURRENCY_SYMBOL }}
+            </div>
+
+            <RouterLink :to="currentUser === undefined ? '/login' : '/book'">
+              <span
+                class="text-link-light dark:text-link-darkfont-bold underline"
+                >Book now</span
+              >
+            </RouterLink>
+          </div>
+        </div>
       </SurfaceCard>
     </section>
   </TransitionRoot>
